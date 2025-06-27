12774b2a85e1881d3d514b6ec700b52190f8decf
Rodr1to <Rodr1to@users.noreply.github.com>
2025-06-26 21:05:37 -0500

    updated routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\MembershipPlanController; // import the controller
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        // Redirects '/admin' to the plans list page for convenience
+        Route::get('/', function () {
+            return redirect()->route('admin.plans.index');
+        });
+
+        Route::resource('plans', MembershipPlanController::class);
 });
 
 require __DIR__.'/auth.php';
